791bfc6c24eaae23351a1b0c729d30800400fc24
Ksenia Galkina <k5eniiagalkina@tandex.ru>
2023-08-09 23:29:22 +0300

    15 project

--- backend/middlewares/cors.js
+++ backend/middlewares/cors.js
@@ -1,6 +1,6 @@
 const allowedCors = [
   'https://kseniag.nomoreparties.co',
-  // 'http://kseniagal.nomoreparties.co',
+  'http://kseniag.nomoreparties.co',
   // 'https://localhost:3000',
   // 'http://localhost:3000',
   // 'https://localhost:3001',
