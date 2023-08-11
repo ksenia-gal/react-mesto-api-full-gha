6b3847a8a5586f224cecc717972834cb95238092
Ksenia Galkina <k5eniiagalkina@tandex.ru>
2023-08-11 23:19:59 +0300

    15 Project

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -15,7 +15,7 @@ const login = (req, res, next) => {
       // создадим токен
       const token = jwt.sign(
         { _id: user._id },
-        NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret',
+        NODE_ENV === 'production' ? JWT_SECRET : 'secret-key',
         { expiresIn: '7d' },
       );
         // вернем токен
