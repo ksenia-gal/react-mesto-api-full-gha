b7cbdb404137f4038d8c4946e1426ebf879d3cd2
Ksenia Galkina <k5eniiagalkina@tandex.ru>
2023-08-11 19:07:35 +0300

    15 Project

--- backend/app.js
+++ backend/app.js
@@ -24,21 +24,17 @@ app.use(helmet());
 
 app.use(express.json());
 
-// app.use(cookieParser());
-
 app.use(bodyParser.json());
 
 app.use(limiter);
 
-app.use(cors);
-
 app.use(requestLogger);
+
+app.use(cors);
 
 app.use(routes);
 
 app.use(errorLogger);
-
-app.use((req, res, next) => next(new NotFoundError('Страница не существует')));
 
 app.use(errors());
 
