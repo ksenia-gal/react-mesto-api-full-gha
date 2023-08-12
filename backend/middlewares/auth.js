9bf21f1ec3d0b8ca1f10ed7bda26358a659f35ca
Ksenia Galkina <k5eniiagalkina@tandex.ru>
2023-08-12 14:19:16 +0300

    15 Project

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -11,9 +11,9 @@ const auth = (req, res, next) => {
   let payload;
   try {
     // попытаемся верифицировать токен
-    payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'secret-key');
+    payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret');
   } catch (err) {
-    // отправим ошибку, если не получилось
+    // отправим ошибку, если не получилось.
     next(new AuthorizationError('Токен не верифицирован, необходима авторизация'));
   }
   req.user = payload; // записываем пейлоуд в объект запроса
