9cdfa1efd4b628cf88de9a102693b81b0ebb4f5d
Ksenia Galkina <k5eniiagalkina@tandex.ru>
2023-08-12 13:18:07 +0300

    15 Project

--- backend/middlewares/errorHandler.js
+++ backend/middlewares/errorHandler.js
@@ -1,23 +1,14 @@
 const { INTERNAL_SERVER_ERROR } = require('../errors/errorsStatusCode');
 
-// const errorHandler = (err, req, res, next) => {
-//   const { statusCode = INTERNAL_SERVER_ERROR, message } = err;
-//   res
-//     .status(statusCode)
-//     .send({
-//       message: statusCode === INTERNAL_SERVER_ERROR
-//         ? 'На сервере произошла ошибка'
-//         : message,
-//     });
-//   next();
-// };
-
-// module.exports = { errorHandler };
-
 const errorHandler = (err, req, res, next) => {
-  const statusCode = INTERNAL_SERVER_ERROR;
-  const message = statusCode === INTERNAL_SERVER_ERROR ? 'На сервере произошла ошибка' : err.message;
-  res.status(statusCode).send({ message });
+  const { statusCode = INTERNAL_SERVER_ERROR, message } = err;
+  res
+    .status(statusCode)
+    .send({
+      message: statusCode === INTERNAL_SERVER_ERROR
+        ? 'На сервере произошла ошибка'
+        : message,
+    });
   next();
 };
 
