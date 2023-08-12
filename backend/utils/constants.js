a87ea6233ef74247c7762e26814fc87801edf3c7
Ksenia Galkina <k5eniiagalkina@tandex.ru>
2023-08-12 21:47:47 +0300

    15 Project

--- backend/utils/constants.js
+++ backend/utils/constants.js
@@ -1,5 +1,7 @@
 const { NODE_ENV, SECRET_SIGNING_KEY } = process.env;
+
 const SECRET_KEY_DEV = 'dev-secret-key';
+
 const REGEX = /https?:\/\/([a-z0-9-]+\.)+[a-z]{2,}([a-zA-Z0-9-._~:?#[\]@!$&'()*+,;=]*)/;
 
 module.exports = {
