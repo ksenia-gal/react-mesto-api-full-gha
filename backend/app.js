f7b2600377349a306439d78998be5ca2f63c24af
Ksenia Galkina <k5eniiagalkina@tandex.ru>
2023-08-13 11:02:35 +0300

    15 Project

--- backend/app.js
+++ backend/app.js
@@ -17,25 +17,23 @@ const { PORT = 3000 } = process.env;
 // создание приложения
 const app = express();
 
+mongoose.connect('mongodb://127.0.0.1:27017/mestodb');
+
 app.use(helmet());
 
 app.use(express.json());
 
 app.use(bodyParser.json());
 
-app.use(limiter);
-
-app.use(cors);
-
-mongoose.connect('mongodb://127.0.0.1:27017/mestodb');
-
 app.use(requestLogger);
-
+app.use(cors);
 app.get('/crash-test', () => {
-  setImmediate(() => {
+  setTimeout(() => {
     throw new Error('Сервер сейчас упадёт');
-  });
+  }, 0);
 });
+
+app.use(limiter);
 
 app.use(routes);
 
